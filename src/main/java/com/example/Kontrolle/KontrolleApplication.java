f2b79a60169d0f0ec03e23dd877e92c4d7feef9c
Kevin Hain <Kevinhainfc@gmail.com>
2020-11-18 13:43:02 +0100

    start

---
+++ src/main/java/com/example/Kontrolle/KontrolleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kontrolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontrolleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontrolleApplication.class, args);
+	}
+
+}
